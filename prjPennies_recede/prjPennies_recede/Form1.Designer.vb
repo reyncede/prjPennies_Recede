ac1ca58e8d43eb43eb9dbfeffa965037780ed7c4
reyncede <69535231+reyncede@users.noreply.github.com>
2020-08-31 01:39:46 +0800

    Add files via upload

---
+++ prjPennies_recede/prjPennies_recede/Form1.Designer.vb
@@ -0,0 +1,225 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPennies = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtDollars = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtDimes = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtQuarters = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtNickels = New System.Windows.Forms.TextBox()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtRemaining = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(62, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Pennies"
+        '
+        'txtPennies
+        '
+        Me.txtPennies.Location = New System.Drawing.Point(59, 54)
+        Me.txtPennies.Name = "txtPennies"
+        Me.txtPennies.Size = New System.Drawing.Size(53, 20)
+        Me.txtPennies.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(19, 89)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Dollars:"
+        '
+        'txtDollars
+        '
+        Me.txtDollars.Enabled = False
+        Me.txtDollars.Location = New System.Drawing.Point(20, 105)
+        Me.txtDollars.Name = "txtDollars"
+        Me.txtDollars.Size = New System.Drawing.Size(53, 20)
+        Me.txtDollars.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(19, 136)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Dimes:"
+        '
+        'txtDimes
+        '
+        Me.txtDimes.Enabled = False
+        Me.txtDimes.Location = New System.Drawing.Point(20, 152)
+        Me.txtDimes.Name = "txtDimes"
+        Me.txtDimes.Size = New System.Drawing.Size(53, 20)
+        Me.txtDimes.TabIndex = 6
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(94, 89)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(50, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Quarters:"
+        '
+        'txtQuarters
+        '
+        Me.txtQuarters.Enabled = False
+        Me.txtQuarters.Location = New System.Drawing.Point(97, 105)
+        Me.txtQuarters.Name = "txtQuarters"
+        Me.txtQuarters.Size = New System.Drawing.Size(53, 20)
+        Me.txtQuarters.TabIndex = 8
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(94, 136)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(45, 13)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Nickels:"
+        '
+        'txtNickels
+        '
+        Me.txtNickels.Enabled = False
+        Me.txtNickels.Location = New System.Drawing.Point(97, 152)
+        Me.txtNickels.Name = "txtNickels"
+        Me.txtNickels.Size = New System.Drawing.Size(53, 20)
+        Me.txtNickels.TabIndex = 10
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(6, 261)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(156, 23)
+        Me.btnCalc.TabIndex = 11
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(6, 290)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 12
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(87, 290)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 13
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtRemaining)
+        Me.GroupBox1.Controls.Add(Me.btnCalc)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.btnExit)
+        Me.GroupBox1.Controls.Add(Me.txtPennies)
+        Me.GroupBox1.Controls.Add(Me.btnClear)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtDollars)
+        Me.GroupBox1.Controls.Add(Me.txtNickels)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.txtDimes)
+        Me.GroupBox1.Controls.Add(Me.txtQuarters)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(171, 323)
+        Me.GroupBox1.TabIndex = 15
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Pennies Calculator"
+        '
+        'txtRemaining
+        '
+        Me.txtRemaining.Enabled = False
+        Me.txtRemaining.Location = New System.Drawing.Point(59, 212)
+        Me.txtRemaining.Name = "txtRemaining"
+        Me.txtRemaining.Size = New System.Drawing.Size(53, 20)
+        Me.txtRemaining.TabIndex = 14
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(39, 196)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(101, 13)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "Pennies Remaining:"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(196, 343)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmMain"
+        Me.Text = "Pennies"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPennies As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtDollars As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtDimes As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtQuarters As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtNickels As TextBox
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtRemaining As TextBox
+End Class
---
+++ prjPennies_recede/prjPennies_recede/Form1.vb
@@ -0,0 +1,41 @@
+﻿Option Strict On
+Option Explicit On
+Option Infer Off
+Public Class frmMain
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        'Declare Variables
+        Dim pennies As Integer
+        Integer.TryParse(txtPennies.Text, pennies)
+
+        'Dollars = 100 pennies , Quarter = 25  pennies, Dimes = 10 pennies, Nickels = 5 pennies
+        Dim dollars As Integer = pennies \ 100
+        Dim remaining As Integer = pennies Mod 100
+        Dim quarter As Integer = remaining \ 25
+        remaining = remaining Mod 25
+        Dim dimes As Integer = remaining \ 10
+        remaining = remaining Mod 10
+        Dim nickels As Integer = remaining \ 5
+
+        'Display Texts
+        txtNickels.Text = Convert.ToString(nickels)
+        txtDimes.Text = Convert.ToString(dimes)
+        txtQuarters.Text = Convert.ToString(quarter)
+        txtDollars.Text = Convert.ToString(dollars)
+        txtRemaining.Text = Convert.ToString(remaining)
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear Textboxes
+        txtPennies.Clear()
+        txtNickels.Clear()
+        txtDimes.Clear()
+        txtQuarters.Clear()
+        txtDollars.Clear()
+        txtRemaining.Clear()
+        txtPennies.Select()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+End Class
